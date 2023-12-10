d9c8a8016ba428f7310f27db5b9f2f4587c1c1c9
abdul <abdulahisholabi@gmail.com>
2023-12-10 15:35:22 -0800

    mv-detail html creation

---
+++ Data.js
@@ -0,0 +1,210 @@
+let Data ;
+
+export default Data =  [
+
+    {
+        "id" : 1 ,
+        "name" : "Fast X" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-01.jpeg" ,
+        "release-date" : 2022 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : ["John cena" , "Jason momoa"] ,
+        "producer" : "abdul"
+    } ,
+        
+    {
+        "id" : 2 ,
+        "name" : "Inception" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 8.5 ,
+        "img" : "./img/mv-1.jpg" ,
+        "release-date" : 2015 ,
+        "box-office" : "2.5 billion" ,
+        "characters" : ["leonardo decaprio"] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 3 ,
+        "name" : "No Time to Die" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 7.5 ,
+        "img" : "./img/mv-02.jpeg" ,
+        "release-date" : 2022 ,
+        "box-office" : "800 million" ,
+        "characters" : ["John cena" , "Jason momoa"] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 4 ,
+        "name" : "Avatar" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 7.5 ,
+        "img" : "./img/mv-2.jpg" ,
+        "release-date" : 2010 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 5 ,
+        "name" : "Doctor Strange" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 7.5 ,
+        "img" : "./img/mv-03.jpeg" ,
+        "release-date" : 2019 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    },
+
+    {
+        "id" : 6 ,
+        "name" : "Thor" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 7.5 ,
+        "img" : "./img/mv-3.jpg" ,
+        "release-date" : 2011 ,
+        "box-office" : "900 million" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 7 ,
+        "name" : "Spiderman" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 7.5 ,
+        "img" : "./img/mv-04.jpeg" ,
+        "release-date" : 2022 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 8 ,
+        "name" : "Godzila" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 7.5 ,
+        "img" : "./img/mv-4.jpg" ,
+        "release-date" : 2020 ,
+        "box-office" : "900 million" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    }
+    ,
+
+    {
+        "id" : 9 ,
+        "name" : "Joker" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-05.jpeg" ,
+        "release-date" : 2019 ,
+        "box-office" : "700 million" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 10 ,
+        "name" : "Joker" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-5.jpg" ,
+        "release-date" : 2015 ,
+        "box-office" : "900 million" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 11 ,
+        "name" : "Alladin" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-06.jpeg" ,
+        "release-date" : 2021 ,
+        "box-office" : "1 billion" ,
+        "characters" : ["will smith"] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 12 ,
+        "name" : "End Game" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 7.5 ,
+        "img" : "./img/mv-07.jpeg" ,
+        "release-date" : 2022 ,
+        "box-office" : "2.5 billion" ,
+        "characters" : ["chris hemsworth"] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 13 ,
+        "name" : "Lion King" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-08.jpeg" ,
+        "release-date" : 2020 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 14 ,
+        "name" : "Star Wars" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-09.jpeg" ,
+        "release-date" : 2018 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 15 ,
+        "name" : "Lion King" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-10.jpeg" ,
+        "release-date" : 2016 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : [] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 16 ,
+        "name" : "The Mummy" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-11.jpeg" ,
+        "release-date" : 2010 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : ["Dwayne Johnsonn"] ,
+        "producer" : "abdul"
+    } ,
+
+    {
+        "id" : 17 ,
+        "name" : "Thor" ,
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deserunt vitae reiciendis vel ",
+        "ratings" : 6.5 ,
+        "img" : "./img/mv-12.jpeg" ,
+        "release-date" : 2021 ,
+        "box-office" : "1.5 billion" ,
+        "characters" : ["Chris hemsworth"] ,
+        "producer" : "abdul"
+    }
+
+]
